6d3885365035d1492ee97763d39e59dd4eb13e56
ukasz09 <246703@student.pwr.edu.pl>
2021-01-06 16:52:59 +0100

    Add navabar, needed dependencies and basic folders structure

--- zpi-assistant/src/app/app.module.ts
+++ zpi-assistant/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './shared/layouts/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    ButtonsModule.forRoot(),
+    FormsModule,
+    ModalModule.forRoot(),
+    TypeaheadModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
